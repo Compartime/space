2aae6ccb1273c671c3872c5e9a46d3863d08c7b3
Compartime <131001257+Compartime@users.noreply.github.com>
2025-08-21 15:15:24 -0300

    Add files via upload

---
+++ bunny/js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+{
+    name: "Hablen lo que Quieran",
+    artist: "Cazzu",
+    img: "img/cazzu.jpg",
+    src: "player canciones/music/Cazzu - Hablen lo que Quieran.mp3"
+},
+{
+    name: "Hola, Qué Tal",
+    artist: "Beret",
+    img: "img/beret.jpg",
+    src: "music/Beret - Hola, qué tal_.mp3"
+},
+{
+    name: "Dime Quien Ama De Verdad",
+    artist: "Karen Méndez",
+    img: "img/karen.jpg",
+    src: "music/Karen Méndez - Dime quien ama de verdad.mp3"
+},
+{
+    name: "Hablen lo que Quieran",
+    artist: "Cazzu",
+    img: "img/cazzu.jpg",
+    src: "music/Cazzu - Hablen lo que Quieran.mp3"
+},
+{
+    name: "Hola, Qué Tal",
+    artist: "Beret",
+    img: "img/beret.jpg",
+    src: "music/Beret - Hola, qué tal_.mp3"
+},
+{
+    name: "Dime Quien Ama De Verdad",
+    artist: "Karen Méndez",
+    img: "img/karen.jpg",
+    src: "music/Karen Méndez - Dime quien ama de verdad.mp3"
+},
+
+];
